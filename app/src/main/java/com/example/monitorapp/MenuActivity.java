35d4c288cd488b82a316f68e7179e2168fd3b208
estebanlago <167945774+estebanlago@users.noreply.github.com>
2024-11-11 19:47:58 -0300

    Avance #3

--- app/src/main/java/com/example/monitorapp/MenuActivity.java
+++ app/src/main/java/com/example/monitorapp/MenuActivity.java
@@ -1,12 +1,12 @@
 package com.example.monitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -14,11 +14,35 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button ubicacionButton = findViewById(R.id.gestionarUbicacionButton);
+        Button sensorButton = findViewById(R.id.gestionarSensorButton);
+        Button historialButton = findViewById(R.id.gestionarHistorialButton);
+
+        sensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, SensoresActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ubicacionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, UbicacionesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, HistorialActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
