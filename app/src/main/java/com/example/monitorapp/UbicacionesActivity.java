7d1f98cabc3d6aa6db460e7737ad83e297dc4975
estebanlago <167945774+estebanlago@users.noreply.github.com>
2024-11-11 21:38:43 -0300

    Avance #4

--- app/src/main/java/com/example/monitorapp/UbicacionesActivity.java
+++ app/src/main/java/com/example/monitorapp/UbicacionesActivity.java
@@ -1,12 +1,14 @@
 package com.example.monitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.monitorapp.CrearUbicacionActivity;
 
 public class UbicacionesActivity extends AppCompatActivity {
 
@@ -14,11 +16,17 @@ public class UbicacionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_ubicaciones);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button agregarUbicacionButton = findViewById(R.id.agregarUbicacionButton);
+
+        agregarUbicacionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UbicacionesActivity.this, CrearUbicacionActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
