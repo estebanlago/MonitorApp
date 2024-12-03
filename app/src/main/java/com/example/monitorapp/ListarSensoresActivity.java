bf8d2d4bc701a8e229d109f0e9c0df5c43da72c2
estebanlago <167945774+estebanlago@users.noreply.github.com>
2024-12-03 19:35:18 -0300

    Avance busqueda sensores

--- app/src/main/java/com/example/monitorapp/ListarSensoresActivity.java
+++ app/src/main/java/com/example/monitorapp/ListarSensoresActivity.java
@@ -1,6 +1,9 @@
 package com.example.monitorapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +13,13 @@ import com.example.monitorapp.adapters.UbicacionesAdapter;
 import com.example.monitorapp.datos.Repositorio;
 import com.example.monitorapp.model.Ubicacion;
 import com.example.monitorapp.model.Sensor;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListarSensoresActivity extends AppCompatActivity {
@@ -27,7 +36,28 @@ public class ListarSensoresActivity extends AppCompatActivity {
         sensoresRecyclerView= findViewById(R.id.listarSensoresRecyclerView);
         sensoresRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        sensores = Repositorio.getInstance().sensores;
+        sensores = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("sensores").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                if (doc != null) {
+                                    Sensor sensor = doc.toObject(Sensor.class);
+                                    sensores.add(sensor);
+                                }
+                            }
+                            sensoresAdapter= new SensoresAdapter(sensores);
+                            sensoresRecyclerView.setAdapter(sensoresAdapter);
+                        } else {
+                            Toast.makeText(ListarSensoresActivity.this,"Error al obtener datos",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
         sensoresAdapter= new SensoresAdapter(sensores);
         sensoresRecyclerView.setAdapter(sensoresAdapter);
     }
