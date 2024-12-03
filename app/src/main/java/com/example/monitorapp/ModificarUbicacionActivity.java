09e46dc282c47e1ba4b8b124d211b862eb4d4681
estebanlago <167945774+estebanlago@users.noreply.github.com>
2024-12-02 23:22:05 -0300

    Avance...

--- app/src/main/java/com/example/monitorapp/ModificarUbicacionActivity.java
+++ app/src/main/java/com/example/monitorapp/ModificarUbicacionActivity.java
@@ -1,24 +1,76 @@
 package com.example.monitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.monitorapp.adapters.UbicacionesAdapter;
+import com.example.monitorapp.datos.Repositorio;
+import com.example.monitorapp.model.Tipo;
+import com.example.monitorapp.model.Ubicacion;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModificarUbicacionActivity extends AppCompatActivity {
+
+    private List<Ubicacion> ubicaciones;
+    private Spinner ubicacionesSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_modificar_ubicacion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        ubicacionesSpinner = findViewById(R.id.nombreUbicacionSpinner);
+
+        ubicaciones = new ArrayList<>();
+
+        ArrayAdapter<Ubicacion> ubicacionAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, ubicaciones);
+
+        ubicacionesSpinner.setAdapter(ubicacionAdapter);
+
+        db.collection("ubicaciones").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                if (doc != null) {
+                                    Ubicacion ubicacion = doc.toObject(Ubicacion.class);
+                                    ubicaciones.add(ubicacion);
+                                }
+                                ubicacionAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(ModificarUbicacionActivity.this,"Error al obtener datos",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+        Ubicacion ubicacion = (Ubicacion) ubicacionesSpinner.getSelectedItem();
     }
 }
